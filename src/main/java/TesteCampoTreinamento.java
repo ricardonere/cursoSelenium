a57e0180a5bc7b5bc718ff8881139710aa6dd0cf
Ricardo <ricardonere@gmail.com>
2022-04-28 13:58:14 -0300

    Teste Valores Combo Multiplo

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -85,15 +85,22 @@ public class TesteCampoTreinamento {
         }
     }
 
-//    @Test
-//    public void deveVerificarValoresComboMultiplo() {
-//        WebDriver driver = new FirefoxDriver();
-//        driver.manage().window().setSize(new Dimension(1200, 765));
-//        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-//        WebElement element = driver.findElement(By.id("elementosForm:esportes"));
-//        Select combo = new Select(element);
-//        combo.selectByVisibleText("Natacao");
-//        combo.selectByVisibleText("Corrida");
-//        combo.selectByVisibleText("O que eh esporte?");
-//    }
+    @Test
+    public void deveVerificarValoresComboMultiplo() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+        Select combo = new Select(element);
+        combo.selectByVisibleText("Natacao");
+        combo.selectByVisibleText("Corrida");
+        combo.selectByVisibleText("O que eh esporte?");
+        List <WebElement> allSelectedOptions = combo.getAllSelectedOptions();
+        Assert.assertEquals(3,allSelectedOptions.size());
+
+        combo.deselectByVisibleText("Corrida");
+       allSelectedOptions = combo.getAllSelectedOptions();
+        Assert.assertEquals(2,allSelectedOptions.size());
+        driver.quit();
+    }
 }
