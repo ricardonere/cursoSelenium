85a351a3b07cf0246d6b5aa2a06e9995983df016
Ricardo <ricardonere@gmail.com>
2022-05-02 15:29:11 -0300

    Teste com Frames

---
+++ src/main/java/TesteFrames.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteFrames {
+    @Test
+    public void deveInteragirComFrames() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(1200, 765));
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+        driver.switchTo().frame("frame1");
+        driver.findElement(By.id("frameButton")).click();
+        Alert alert = driver.switchTo().alert();
+        String msg = alert.getText();
+        Assert.assertEquals("Frame OK!", msg);
+        alert.accept();
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys();
+
+
+    }
+}
